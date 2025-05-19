d4aeb6fab0ae632acaa745562961b812d6898e9e
liquid <adhikariaakash90@gmail.com>
2025-05-19 17:45:44 +0545

    Added a context struct and a context getter function

---
+++ CMakeFiles/8085.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/8085.dir/link.d"
+  "CMakeFiles/8085.dir/src/8085.cpp.o"
+  "CMakeFiles/8085.dir/src/8085.cpp.o.d"
+  "CMakeFiles/8085.dir/src/Execute.cpp.o"
+  "CMakeFiles/8085.dir/src/Execute.cpp.o.d"
+  "CMakeFiles/8085.dir/src/Instruction.cpp.o"
+  "CMakeFiles/8085.dir/src/Instruction.cpp.o.d"
+  "CMakeFiles/8085.dir/src/Transfer.cpp.o"
+  "CMakeFiles/8085.dir/src/Transfer.cpp.o.d"
+  "lib8085.pdb"
+  "lib8085.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/8085.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/terminal.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/terminal.dir/link.d"
+  "CMakeFiles/terminal.dir/src/Terminal.cpp.o"
+  "CMakeFiles/terminal.dir/src/Terminal.cpp.o.d"
+  "libterminal.pdb"
+  "libterminal.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/terminal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
